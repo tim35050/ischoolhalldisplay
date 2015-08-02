7010fd8742689547e5601c3a400169860c80fa68
Tim <tim35050@gmail.com>
2015-08-01 23:07:26 -0700

    constrained slide dimensions, embedded Freight font

--- index.php
+++ index.php
@@ -4,30 +4,31 @@
     <title>iPlasmatron</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>
     <link rel="stylesheet" type="text/css" href="css/style.css"></link>
-    <!-- include jQuery -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-
-    <!-- include Cycle2 -->
-    <script src="js/jquery.cycle2.min.js"></script>
-
 </head>
 <body>
+    <div id="container">
 
-<?php 
+        <?php
+        include 'parser.php';
 
-    include 'parser.php';
+        echo "<div class=\"cycle-slideshow composite\" 
+            data-cycle-fx=\"".$CYCLE_FX."\" 
+            data-cycle-slides=\"> div\"
+            data-cycle-speed=\"".$CYCLE_SPEED."\"
+            data-cycle-timeout=\"".$CYCLE_TIMEOUT."\">";
 
-    echo "<div class=\"cycle-slideshow composite\" 
-        data-cycle-fx=\"".$CYCLE_FX."\" 
-        data-cycle-slides=\"> div\"
-        data-cycle-speed=\"".$CYCLE_SPEED."\"
-        data-cycle-timeout=\"".$CYCLE_TIMEOUT."\">";
+        echo $slides_combined;
 
-    echo $slides_combined;
-
-    echo "</div>";
-?>
-    
-
+        echo "</div>";
+        ?>
+        
+    </div>
+    <!-- include jQuery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <!-- include Cycle2 -->
+    <script src="js/jquery.cycle2.min.js"></script>
+    <!-- include TypeKit fonts -->
+    <script src="//use.typekit.net/dpg6kwf.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
 </body>
 </html>
--- parser.php
+++ parser.php
@@ -4,11 +4,16 @@
 $PROFILES_URL = "http://www.ischool.berkeley.edu/feeds/iplasmatron-profiles";
 $PROJECTS_URL = "http://www.ischool.berkeley.edu/feeds/iplasmatron-mims-projects";
 
-// SLIDESHOW PARAMETERS
+// CYCLE2 PARAMETERS
 // see api here: http://jquery.malsup.com/cycle2/api/
 $CYCLE_FX = "scrollHorz";
 $CYCLE_SPEED = 800;
 $CYCLE_TIMEOUT = 5000;
+
+// SLIDESHOW PARAMETERS
+$NUM_SLIDES = 1; // -1 for all slides
+$ONLY_PROFILES = False;
+$ONLY_PROJECTS = True;
 
 // Load feed into variables
 $profiles_xml = simplexml_load_file($PROFILES_URL) or die("Error: Cannot create profiles object");
@@ -16,42 +21,50 @@ $projects_xml = simplexml_load_file($PROJECTS_URL) or die("Error: Cannot create 
 
 $slides = array();
 
-foreach($profiles_xml->children() as $node) { 
-    $format = 
-    	'<div class="slide-container">
-            <img src="%s">
-            <div class="text-container">
-                <div class="title">%s</div>
-                <div class="teaser-text">%s</div>
-            </div>
-        </div>';
-	$slides[] = sprintf($format, 
-						$node->teaser_image_original, 
-						$node->title, 
-						$node->teaser_text);
+if (!$ONLY_PROJECTS) {
+    foreach($profiles_xml->children() as $node) { 
+        $format = 
+            '<div class="slide-container">
+                <img src="%s">
+                <div class="text-container">
+                    <div class="title">%s</div>
+                    <div class="teaser-text">%s</div>
+                </div>
+            </div>';
+        $slides[] = sprintf($format, 
+                            $node->teaser_image_original, 
+                            $node->title, 
+                            $node->teaser_text);
+    }
 }
 
-foreach($projects_xml->children() as $node) { 
-
-    $format = 
-    	'<div class="slide-container">
-            <img src="%s">
-            <div class="text-container">
-                <div class="title">%s</div>
-                <div class="teaser-text">%s</div>
-                <div class="students">%s</div>
-                <div class="year">%s</div>
-            </div>
-        </div>';
-	$slides[] = sprintf($format, 
-						$node->teaser_image_original, 
-						$node->title, 
-						$node->teaser_text,
-						$node->students,
-						$node->year);
+if (!$ONLY_PROFILES) {
+    foreach($projects_xml->children() as $node) { 
+        $format = 
+            '<div class="slide-container">
+                <img class="projects" src="%s">
+                <div class="text-container">
+                    <div class="title projects">%s</div>
+                    <div class="teaser-text projects">%s</div>
+                    <div class="year">%s</div>
+                    <div class="students">%s</div>
+                </div>
+            </div>';
+        $slides[] = sprintf($format, 
+                            $node->teaser_image_original, 
+                            $node->title, 
+                            $node->teaser_text,
+                            $node->year,
+                            $node->students);
+    }
 }
+
 
 shuffle($slides);
+
+if ($NUM_SLIDES > -1) {
+    $slides = array_slice($slides, 0, $NUM_SLIDES);
+}
+
 $slides_combined = implode('', $slides);
 
-?>
