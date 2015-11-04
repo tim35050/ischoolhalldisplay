fa019585832e2f5d0858d2811968aef9d23c1127
Tim <tim35050@gmail.com>
2015-11-03 20:59:53 -0800

    Increased dimensions to match screen size/resolution, changed background and text color, added images as div backgrounds for cropping, changed transition speed to allow image preloading

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <title>iPlasmatron</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>
+    <meta http-equiv="refresh" content="7200">
     <link rel="stylesheet" type="text/css" href="css/style.css"></link>
 </head>
 <body>
--- parser.php
+++ parser.php
@@ -2,13 +2,14 @@
 
 // FEED PARAMETERS
 $PROFILES_URL = "http://www.ischool.berkeley.edu/feeds/iplasmatron-profiles";
-$PROJECTS_URL = "http://www.ischool.berkeley.edu/feeds/iplasmatron-mims-projects";
+$MIMS_PROJECTS_URL = "http://www.ischool.berkeley.edu/feeds/iplasmatron-mims-projects";
+$MIDS_PROJECTS_URL = "http://www.ischool.berkeley.edu/feeds/iplasmatron-mids-projects";
 
 // CYCLE2 PARAMETERS
 // see api here: http://jquery.malsup.com/cycle2/api/
-$CYCLE_FX = "scrollHorz";
-$CYCLE_SPEED = 800;
-$CYCLE_TIMEOUT = 10000;
+$CYCLE_FX = "none";
+$CYCLE_SPEED = 10 * 1000; // 10 seconds
+$CYCLE_TIMEOUT = 50 * 1000; // 50 seconds
 
 // SLIDESHOW PARAMETERS
 $NUM_SLIDES = -1; // -1 for all slides
@@ -17,32 +18,34 @@ $ONLY_PROJECTS = False;
 
 // Load feed into variables
 $profiles_xml = simplexml_load_file($PROFILES_URL) or die("Error: Cannot create profiles object");
-$projects_xml = simplexml_load_file($PROJECTS_URL) or die("Error: Cannot create projects object");
+$projects_xml = simplexml_load_file($MIMS_PROJECTS_URL) or die("Error: Cannot create projects object");
+//$mids_projects_xml = simplexml_load_file($MIDS_PROJECTS_URL) or die("Error: Cannot create projects object");
+//$projects_xml = array_merge($mims_projects_xml->children(), $mids_projects_xml->children());
 
 $slides = array();
 
 if (!$ONLY_PROJECTS) {
-    foreach($profiles_xml->children() as $node) { 
-        $format = 
-            '<div class="slide-container">
-                <img src="%s">
+    foreach($profiles_xml->children() as $node) {
+        $format =
+            '<div class="slide-container profile">
+                <div class="image-container" style="background-image: url(\'%s\')"></div>
                 <div class="text-container">
                     <div class="title">%s</div>
                     <div class="teaser-text">%s</div>
                 </div>
             </div>';
-        $slides[] = sprintf($format, 
-                            $node->teaser_image_original, 
-                            $node->title, 
+        $slides[] = sprintf($format,
+                            $node->teaser_image_original,
+                            $node->title,
                             $node->teaser_text);
     }
 }
 
 if (!$ONLY_PROFILES) {
-    foreach($projects_xml->children() as $node) { 
-        $format = 
-            '<div class="slide-container">
-                <img class="projects" src="%s">
+    foreach($projects_xml->children() as $node) {
+        $format =
+            '<div class="slide-container project">
+                <div class="image-container" style="background-image: url(\'%s\')"></div>
                 <div class="text-container">
                     <div class="title projects">%s</div>
                     <div class="teaser-text projects">%s</div>
@@ -50,9 +53,9 @@ if (!$ONLY_PROFILES) {
                     <div class="students">%s</div>
                 </div>
             </div>';
-        $slides[] = sprintf($format, 
-                            $node->teaser_image_original, 
-                            $node->title, 
+        $slides[] = sprintf($format,
+                            $node->teaser_image_original,
+                            $node->title,
                             $node->teaser_text,
                             $node->year,
                             $node->students);
@@ -67,4 +70,3 @@ if ($NUM_SLIDES > -1) {
 }
 
 $slides_combined = implode('', $slides);
-
